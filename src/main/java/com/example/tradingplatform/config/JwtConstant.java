03d627c3f51daa13d4b3a5b11d987ba677fca1b0
Madan-bot05 <madanmohandas983@gmail.com>
2024-08-08 16:25:50 +0530

    implemented security

---
+++ src/main/java/com/example/tradingplatform/config/JwtConstant.java
@@ -0,0 +1,6 @@
+package com.example.tradingplatform.config;
+
+public class JwtConstant {
+    public static final String SECRET_KEY = "zesxrdctfvgybhunjimokertfyguhitf7gy8huojtfyeguhijwopoavhfjdskcnlmfcwtvgubhivf";
+    public static  final String JWT_HEADER="Authorization";
+}
