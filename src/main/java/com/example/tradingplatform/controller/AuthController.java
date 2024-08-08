f7ef408ee1f0ec43ca8fcdd9cf65393a138f45e7
Madan-bot05 <madanmohandas983@gmail.com>
2024-08-08 20:46:28 +0530

    Login Sucessfully implemented

--- src/main/java/com/example/tradingplatform/controller/AuthController.java
+++ src/main/java/com/example/tradingplatform/controller/AuthController.java
@@ -5,12 +5,14 @@ import com.example.tradingplatform.config.JwtProvider;
 import com.example.tradingplatform.model.User;
 import com.example.tradingplatform.repository.UserRepository;
 import com.example.tradingplatform.response.AuthResponse;
+import com.example.tradingplatform.service.CustomUserDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,6 +23,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class AuthController {
     @Autowired
     private UserRepository userRepository;
+    @Autowired
+    private CustomUserDetailsService customUserDetailsService;
 
     @PostMapping("/signup")
     public ResponseEntity<AuthResponse> register (@RequestBody User user) throws Exception {
@@ -45,5 +49,31 @@ public class AuthController {
         res.setStatus(true);
         res.setMessage("Regsiter Success");
         return new ResponseEntity<>(res, HttpStatus.CREATED);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<AuthResponse> login (@RequestBody User user) throws Exception {
+        String username=user.getEmail();
+        String password=user.getPassword();
+        Authentication auth=authenticate(username,password);
+
+
+        String jwt= JwtProvider.generateToken(auth);
+        AuthResponse res=new AuthResponse();
+        res.setJwt(jwt);
+        res.setStatus(true);
+        res.setMessage("login Success");
+        return new ResponseEntity<>(res, HttpStatus.CREATED);
+    }
+
+    private Authentication authenticate(String username, String password) throws Exception {
+        UserDetails userDetails=customUserDetailsService.loadUserByUsername(username);
+        if(userDetails==null){
+            throw new Exception("Invalid Username");
+        }
+        if(!password.equals(userDetails.getPassword())){
+            throw new Exception("Invalid Password");
+        }
+        return new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
     }
 }
